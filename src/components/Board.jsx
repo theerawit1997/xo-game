a75c5d0ca3a5412cbcac92cace9b2854cc7c2387
theerawit1997 <79015789+theerawit1997@users.noreply.github.com>
2023-10-07 15:54:23 +0700

    make new

---
+++ src/components/Board.jsx
@@ -0,0 +1,9 @@
+function Board() {
+  return (
+    <>
+      <h4>Board Game XO</h4>
+    </>
+  );
+}
+
+export default Board;
---
+++ src/components/TicTacToe.jsx
@@ -0,0 +1,12 @@
+import Board from "./Board";
+
+function TicTacToe() {
+  return (
+    <>
+      <h1>XO Game</h1>
+      <Board />
+    </>
+  );
+}
+
+export default TicTacToe;
