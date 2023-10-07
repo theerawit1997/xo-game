24d16bbdfc73ac3eadf93f510b61951da380550d
theerawit1997 <79015789+theerawit1997@users.noreply.github.com>
2023-10-07 17:28:35 +0700

    Winner line

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -1,20 +1,53 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Board from "./Board";
 
 const PlayerX = "X";
 const PlayerO = "O";
 
+const winningCombinations = [
+  //rows
+  { combo: [0, 1, 2], strikeClass: "strike-row-1" },
+  { combo: [3, 4, 5], strikeClass: "strike-row-2" },
+  { combo: [6, 7, 8], strikeClass: "strike-row-3" },
+
+  //columns
+  { combo: [0, 3, 6], strikeClass: "strike-column-1" },
+  { combo: [1, 4, 7], strikeClass: "strike-column-2" },
+  { combo: [2, 5, 8], strikeClass: "strike-column-3" },
+
+  //diagonals
+  { combo: [0, 4, 8], strikeClass: "strike-diagonal-1" },
+  { combo: [2, 4, 6], strikeClass: "strike-diagonal-2" },
+];
+
+function checkWinner(tiles, setStrikeClass) {
+  // console.log("check winner");
+  for (const { combo, strikeClass } of winningCombinations) {
+    const tileValue1 = tiles[combo[0]];
+    const tileValue2 = tiles[combo[1]];
+    const tileValue3 = tiles[combo[2]];
+
+    if (
+      tileValue1 !== null &&
+      tileValue1 === tileValue2 &&
+      tileValue1 === tileValue3
+    ) {
+      setStrikeClass(strikeClass);
+    }
+  }
+}
+
 function TicTacToe() {
   const [tiles, setTiles] = useState(Array(9).fill(null));
   const [playerTurn, setPlayerTurn] = useState(PlayerX);
-  const [strikeClass, setstrikeClass] = useState();
+  const [strikeClass, setStrikeClass] = useState();
 
   const handleTileClick = (index) => {
     if (tiles[index] !== null) {
       return;
     }
 
-    console.log(index);
+    // console.log(index);
     const newTiles = [...tiles];
     newTiles[index] = playerTurn;
     setTiles(newTiles);
@@ -24,6 +57,10 @@ function TicTacToe() {
       setPlayerTurn(PlayerX);
     }
   };
+
+  useEffect(() => {
+    checkWinner(tiles, setStrikeClass);
+  }, [tiles]);
 
   return (
     <>
